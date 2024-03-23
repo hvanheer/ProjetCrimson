770e7377357506d6912f44db161cd02206a75213
AugustinDns <augustin.denis@epfedu.fr>
2024-03-23 14:51:51 +0100

    commit du projet vierge swift pour l'interface

---
+++ ProjetCrimson/interfaceSwift/crimsonInterface/crimsonInterface/crimsonInterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  crimsonInterfaceApp.swift
+//  crimsonInterface
+//
+//  Created by Augustin DENIS on 23/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct crimsonInterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
