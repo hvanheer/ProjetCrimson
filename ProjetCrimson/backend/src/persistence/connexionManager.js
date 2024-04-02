da6faab63a8fa515b6d7e8be91833d77a3c5220e
ewarskotte <elea.warskotte@epfedu.fr>
2024-04-02 14:29:12 +0200

    Correction connexionManager

--- ProjetCrimson/backend/src/persistence/connexionManager.js
+++ ProjetCrimson/backend/src/persistence/connexionManager.js
@@ -1,26 +1,23 @@
 const { makeDb } = require('mysql-async-simple');
 const mysql = require('mysql');
 
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "nom_utilisateur",
+    password: "mot_de_passe_utilisateur"
+});
+
+const db = null;
+
 class connexionManager {
-
-    const connection = mysql.createConnection({
-        host: "localhost",
-        user: "nom_utilisateur",
-        password: "mot_de_passe_utilisateur"
-    });
-
-    const db = null;
-
-    async function initDB() {
+    async initDB() {
         if (db === null) {
             const db = makeDb();
             await db.connect(connection);
         }
     }
-
-    export async function getConnexion() {
-        await this.initDB();
-        return [db, connection];
-    }
-
+}
+export async function getConnexion() {
+    await this.initDB();
+    return [db, connection];
 }
