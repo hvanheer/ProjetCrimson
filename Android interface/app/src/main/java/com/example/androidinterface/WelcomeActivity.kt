e7938e5875038f47c6a6854e54d7e49db8cd6bc7
unknown <yangasa.maxence@outlook.fr>
2024-04-11 11:30:28 +0200

    Connexion Api spotify

--- Android interface/app/src/main/java/com/example/androidinterface/WelcomeActivity.kt
+++ Android interface/app/src/main/java/com/example/androidinterface/WelcomeActivity.kt
@@ -1,10 +1,19 @@
 package com.example.androidinterface
+
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.IOException
+import java.net.URL
 
 class WelcomeActivity : AppCompatActivity() {
 
@@ -23,6 +32,7 @@ class WelcomeActivity : AppCompatActivity() {
         buttonSpotify.setOnClickListener {
             val pseudo = editTextName.text.toString()
             if (pseudo.isNotBlank()) {
+                connexionSpotify(this)
                 Log.d("Pseudo", "Pseudo: $pseudo - Action: Se connecter à Spotify")
                 editTextName.text.clear()
             } else {
@@ -33,12 +43,69 @@ class WelcomeActivity : AppCompatActivity() {
         buttonDeezer.setOnClickListener {
             val pseudo = editTextName.text.toString()
             if (pseudo.isNotBlank()) {
+                connexionDeezer(this)
                 Log.d("Pseudo", "Pseudo: $pseudo - Action: Se connecter à Deezer")
                 editTextName.text.clear()
+                // Mettez votre action de connexion à Deezer ici
             } else {
                 Toast.makeText(this, "Veuillez entrer un pseudo", Toast.LENGTH_SHORT).show()
             }
         }
     }
-}
 
+    private fun connexionSpotify(activity: AppCompatActivity) {
+
+        val url = URL("http://192.168.1.5:3000/connectAPI")
+        GlobalScope.launch(Dispatchers.IO) {
+            try {
+                val connection = url.openConnection()
+                val data = connection.getInputStream().bufferedReader().readText()
+                val responseURL = Uri.parse(data)
+
+                withContext(Dispatchers.Main) {
+                    val intent = Intent(Intent.ACTION_VIEW, responseURL)
+                    intent.setPackage("com.google.android.apps.chrome")
+
+                    if (intent.resolveActivity(activity.packageManager) != null) {
+                        activity.startActivity(intent)
+                    } else {
+
+                        intent.setPackage(null)
+                        activity.startActivity(intent)
+                    }
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun connexionDeezer(activity: AppCompatActivity) {
+
+        val url = URL("http://192.168.1.5:3000/connectAPI")
+        GlobalScope.launch(Dispatchers.IO) {
+            try {
+                val connection = url.openConnection()
+                val data = connection.getInputStream().bufferedReader().readText()
+                val responseURL = Uri.parse(data)
+
+                withContext(Dispatchers.Main) {
+                    val intent = Intent(Intent.ACTION_VIEW, responseURL)
+                    intent.setPackage("com.google.android.apps.chrome") // Spécifier l'application Google à utiliser
+
+                    if (intent.resolveActivity(activity.packageManager) != null) {
+                        activity.startActivity(intent)
+                    } else {
+
+                        intent.setPackage(null)
+                        activity.startActivity(intent)
+                    }
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+
+}
