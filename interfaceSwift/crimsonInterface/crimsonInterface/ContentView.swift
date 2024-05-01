cf9ebd11844321ac9894a162e2bd7c027e440c85
AugustinDns <augustin.denis@epfedu.fr>
2024-05-01 19:16:53 +0200

    truc qui marche

--- interfaceSwift/crimsonInterface/crimsonInterface/ContentView.swift
+++ interfaceSwift/crimsonInterface/crimsonInterface/ContentView.swift
@@ -9,63 +9,82 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var username: String = ""
-    
+    struct TrackInfo: Codable {
+        let name: String
+        let artist: String
+        let albumArtwork: String
+        // Ajoutez d'autres propriétés si nécessaire
+    }
+
     var body: some View {
-        ZStack {
-            LinearGradient(gradient: Gradient(colors: [Color(hex: 0x660033), Color(hex: 0x0066CC)]), startPoint: .top, endPoint: .bottom)
-                .edgesIgnoringSafeArea(.all)
-            
-            VStack {
-                Text("CRIMSON")
-                    .font(.custom("Phosphate", size: 40))
-                    .foregroundColor(.white)
-                    .padding(.top, 50)
-                
-                TextField("Entrer votre pseudo", text: $username)
-                    .padding()
-                    .background(Color.white)
-                    .cornerRadius(8)
-                    .padding()
-                    .foregroundColor(.black)
-                Text("Connectez vous sur votre plateforme de streaming: ")
-                    .bold()
-                    .foregroundStyle(.white)
-                    .padding()
-                    .multilineTextAlignment(.center)
-                //SPOTIFY//////////////////////////////////////////////////////////////////
-                Button(action: {
-                    crimsonInterfaceApp.connexionSpotify() // Appel de la fonction depuis l'extension de crimsonInterfaceApp
-                }) {
-                    HStack {
-                        Image("spotifylogo")
-                            .resizable()
-                            .frame(width: 20, height: 20) //
-                        Text("Se connecter via Spotify")
-                            .foregroundColor(.white)
+        NavigationView {
+            ZStack {
+                LinearGradient(gradient: Gradient(colors: [Color(hex: 0x660033), Color(hex: 0x0066CC)]), startPoint: .top, endPoint: .bottom)
+                    .edgesIgnoringSafeArea(.all)
+
+                VStack {
+                    Text("CRIMSON")
+                        .font(.custom("Phosphate", size: 40))
+                        .foregroundColor(.white)
+                        .padding(.top, 50)
+
+                    TextField("Entrer votre pseudo", text: $username)
+                        .padding()
+                        .background(Color.white)
+                        .cornerRadius(8)
+                        .padding()
+                        .foregroundColor(.black)
+
+                    Text("Connectez-vous sur votre plateforme de streaming:")
+                        .bold()
+                        .foregroundColor(.white)
+                        .padding()
+                        .multilineTextAlignment(.center)
+
+                    Button(action: {
+                        // Action à effectuer lors du clic sur le bouton Spotify
+                        crimsonInterfaceApp.connexionSpotify()
+                    }) {
+                        HStack {
+                            Image("spotifylogo")
+                                .resizable()
+                                .frame(width: 20, height: 20)
+                            Text("Se connecter via Spotify")
+                                .foregroundColor(.white)
+                        }
+                        .padding()
+                        .background(Color.green)
+                        .cornerRadius(8)
+                    }
+
+                    Button(action: {
+                        // Action à effectuer lors du clic sur le bouton Deezer
+                        crimsonInterfaceApp.connexionDeezer()
+                    }) {
+                        HStack {
+                            Image("deezerlogo")
+                                .resizable()
+                                .frame(width: 20, height: 20)
+                            Text("Se connecter via Deezer")
+                                .foregroundColor(.white)
+                        }
+                        .padding()
+                        .background(Color.purple)
+                        .cornerRadius(8)
                     }
                     .padding()
-                    .background(Color.green)
-                    .cornerRadius(8)
-                }
-                
-                //DEEZER//////////////////////////////////////////////////////////////////
-                Button(action: {
-                    // Action à effectuer lors du clic sur le bouton Deezer
-                    crimsonInterfaceApp.connexionDeezer() // Appel de la fonction depuis l'extension de crimsonInterfaceApp
-                }) {
-                    HStack {
-                        Image("deezerlogo")
-                            .resizable()
-                            .frame(width: 20, height: 20) //
-                        Text("Se connecter via Deezer")
+
+                    NavigationLink(destination: MusicPlayerView(track: MusicTrack(name: "Example Track", artist: "Example Artist", albumArtwork: "album_cover"))) {
+                        Text("Go to Music Player")
                             .foregroundColor(.white)
+                            .padding()
+                            .background(Color.blue)
+                            .cornerRadius(8)
                     }
                     .padding()
-                    .background(Color.purple)
-                    .cornerRadius(8)
                 }
-                .padding()
             }
+            .navigationBarHidden(true)
         }
     }
 }
@@ -75,6 +94,7 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
 
 extension Color {
     init(hex: UInt, alpha: Double = 1.0) {
--- interfaceSwift/crimsonInterface/crimsonInterface/crimsonInterfaceApp.swift
+++ interfaceSwift/crimsonInterface/crimsonInterface/crimsonInterfaceApp.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 import SafariServices
 
+struct TrackInfo: Codable {
+    let name: String
+    let artist: String
+    let albumArtwork: String
+}
+
 @main
 struct crimsonInterfaceApp: App {
     var body: some Scene {
@@ -30,10 +36,14 @@ extension crimsonInterfaceApp {
                         let safariViewController = SFSafariViewController(url: responseURL)
                         UIApplication.shared.windows.first?.rootViewController?.present(safariViewController, animated: true, completion: nil)
                     }
+                    
+                    // Imprimer la réponse de l'API dans la console
+                    print("Réponse de l'API : \(responseString)")
                 }
             }
         }.resume()
     }
+
 
     
     static func connexionDeezer() {
@@ -42,5 +52,36 @@ extension crimsonInterfaceApp {
         let safariViewController = SFSafariViewController(url: url)
         UIApplication.shared.windows.first?.rootViewController?.present(safariViewController, animated: true, completion: nil)
     }
-
+    static func playMusic() {
+           guard let url = URL(string: "http://192.168.1.5:3000/play") else { return }
+           
+           URLSession.shared.dataTask(with: url) { data, response, error in
+               if let error = error {
+                   print("Erreur de requête : \(error.localizedDescription)")
+               } else if let response = response as? HTTPURLResponse {
+                   if response.statusCode == 200 {
+                       print("Lecture de la musique lancée avec succès.")
+                   } else {
+                       print("Erreur lors de la requête : \(response.statusCode)")
+                   }
+               }
+           }.resume()
+       }
+    static func pauseMusic() {
+           guard let url = URL(string: "http://192.168.1.5:3000/pause") else { return }
+           
+           URLSession.shared.dataTask(with: url) { data, response, error in
+               if let error = error {
+                   print("Erreur de requête : \(error.localizedDescription)")
+               } else if let response = response as? HTTPURLResponse {
+                   if response.statusCode == 200 {
+                       print("Pause de la musique lancée avec succès.")
+                   } else {
+                       print("Erreur lors de la requête : \(response.statusCode)")
+                   }
+               }
+           }.resume()
+       }
+        
 }
+
