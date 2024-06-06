9305fcc54544e577ee1c1f2ba3d227da156796e7
ewarskotte <elea.warskotte@epfedu.fr>
2024-06-06 13:52:11 +0200

    maj de la db avec une table de lien entre song et joueur

---
+++ backend/src/dao/SongPlayerLinkDAO.js
@@ -0,0 +1,56 @@
+const connexionManager = require("../persistence/connexionManager");
+
+class SongPlayerLinkDAO {
+
+    constructor() {
+        this.connexionManager = new connexionManager();
+    }
+
+    async createSongPlayerLink(songPlayerLink) {
+        try {
+            const db = await this.connexionManager.getDbConnection();
+            return await db.query(this.connexionManager.connection, 'INSERT INTO songs_players_link(playerID, songID) VALUES(?, ?)', [songPlayerLink.player_id, songPlayerLink.songs_id]);
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    async findSongPlayerLinkByPlayerId(playerID) {
+        try {
+            const db = await this.connexionManager.getDbConnection();
+            const songPlayerLinks = await db.query(this.connexionManager.connection, 'SELECT * FROM songs_players_link WHERE playerID = ?', [playerID]);
+            return songPlayerLinks[0];
+        } catch (err) {
+            throw err;
+        }
+    }
+    async findSongPlayerLinkBySongId(songID) {
+        try {
+            const db = await this.connexionManager.getDbConnection();
+            const songPlayerLinks = await db.query(this.connexionManager.connection, 'SELECT * FROM songs_players_link WHERE songID = ?', [songID]);
+            return songPlayerLinks[0];
+        } catch (err) {
+            throw err;
+        }
+    }
+
+
+    async findAllSongPlayerLink() {
+        try {
+            const db = await this.connexionManager.getDbConnection();
+            return await db.query(this.connexionManager.connection, 'SELECT * FROM songs_players_link');
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    async closeDBConnection() {
+        try {
+            await this.connexionManager.closeDB();
+        } catch (err) {
+            throw err;
+        }
+    }
+}
+
+module.exports = SongPlayerLinkDAO;
---
+++ backend/src/service/SongPlayerLinkService.js
@@ -0,0 +1,48 @@
+const SongPlayerLinkDAO = require('../dao/SongPlayerLinkDAO');
+
+class SongPlayerLinkService {
+    constructor() {
+        this.songPlayerLinkDAO = new SongPlayerLinkDAO();
+    }
+
+    async createSongPlayerLink(songPlayerLink) {
+        try {
+            return await this.songPlayerLinkDAO.createSongPlayerLink(songPlayerLink);
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    async findSongPlayerLinkByPlayerId(playerID) {
+        try {
+            return await this.songPlayerLinkDAO.findSongPlayerLinkByPlayerId(playerID);
+        } catch (err) {
+            throw err;
+        }
+    }
+    async findSongPlayerLinkBySongId(songID) {
+        try {
+            return await this.songPlayerLinkDAO.findSongPlayerLinkBySongId(songID);
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    async findAllSongPlayerLink() {
+        try {
+            return await this.songPlayerLinkDAO.findAllSongPlayerLink();
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    async closeDBConnection() {
+        try {
+            await this.songPlayerLinkDAO.closeDBConnection();
+        } catch (err) {
+            throw err;
+        }
+    }
+}
+
+module.exports = SongPlayerLinkService;
