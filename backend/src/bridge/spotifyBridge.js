bb318d94cbd7e6556b2503a60d7f934e1db8d448
hvanheer <hugo.vanheerden@epfedu.fr>
2024-06-13 12:44:29 +0200

    trying to fix

--- backend/src/bridge/spotifyBridge.js
+++ backend/src/bridge/spotifyBridge.js
@@ -170,12 +170,12 @@ async function getUserData(access_token) {
 }
 
 app.get('/play', async (req, res) => {
-    console.log('Session data:', req.session);
-    console.log('User name:', req.session.user);
-    if (!req.session.user) {
+    //console.log('Session data:', req.session);
+    //console.log('User name:', req.session.user);
+    /*if (!req.session.user) {
         res.status(401).send('User not authenticated');
         return;
-    }
+    }*/
 
     try {
         const data = await readFileAsync('top_tracks.json');
